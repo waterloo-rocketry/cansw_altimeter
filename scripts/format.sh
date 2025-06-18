06f25194fd467dc328f54fdaab6f2e446873a5ce
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-06-18 18:50:40 -0400

    Add a GitHub Action to check C++ coding style

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-${CLANG_FORMAT:-clang-format} -i --style=file:third_party/rocketlib/.clang-format src/apps/*/*.cpp src/apps/*/*.hpp src/drivers/*/*.cpp src/drivers/*/*.hpp 
+${CLANG_FORMAT:-clang-format} -i --style=file:third_party/rocketlib/.clang-format src/apps/*/*.cpp src/apps/*/*.hpp src/drivers/*/*.cpp src/drivers/*/*.hpp
