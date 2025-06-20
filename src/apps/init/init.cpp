f81417b270d86438758f7d63dd04af37d925ca73
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-06-20 17:57:58 -0400

    Add initial initialization code

--- src/apps/init/init.cpp
+++ src/apps/init/init.cpp
@@ -1,2 +1,25 @@
+#include "canlib.h"
+#include "common.h"
+#include "main.h"
+
 #include "init.hpp"
 
+extern FDCAN_HandleTypeDef hfdcan1;
+
+void can_callback_function(const can_msg_t *message, uint32_t) {
+    switch (get_message_type(message)) {
+        case MSG_RESET_CMD:
+            if (check_board_need_reset(message)) {
+                NVIC_SystemReset();
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+extern "C" w_status_t system_init(void) {
+    can_init_stm(&hfdcan1, can_callback_function);
+
+    return W_SUCCESS;
+}
---
+++ src/apps/init/init.h
@@ -0,0 +1,11 @@
+// This file is used as a C to C++ bridge, to be used in main.c StartDefaultTask function
+// Only system_init function is needed
+
+#ifndef APPS_INIT_INIT_H
+#define APPS_INIT_INIT_H
+
+#include "common.h"
+
+w_status_t system_init(void);
+
+#endif
